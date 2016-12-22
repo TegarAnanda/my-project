cd5da69c2763436f567fb19a6687d4c93cbe6174
TegarAnanda <tegar.ananda77@gmail.com>
2016-12-22 18:00:59 +0700

    first commit

---
+++ database/migrations/2016_12_22_103052_create_threads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('judul');
+            $table->string('isi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('threads');
+    }
+}
